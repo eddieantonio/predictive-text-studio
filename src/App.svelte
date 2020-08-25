127c1233befee82e140f779e9c85c09151722b36
Eddie Antonio Santos <easantos@ualberta.ca>
2020-08-25 11:46:20 -0600

    feat: Use Svelte to render front page

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script>
+  export let name;
+</script>
+
+<main role="app">
+  <h1> Welcome to {name}!</h1>
+</main>
--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-console.log("Hello, World!");
+import App from './App.svelte';
+
+const app = new App({
+  target: document.body,
+  props: {
+    name: 'Predictive Text Studio',
+  }
+});
+
+export default app;
