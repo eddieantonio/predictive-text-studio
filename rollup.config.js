0a2f79b8463d57e48bcdce7d86e547b846fd3cf2
Eddie Antonio Santos <easantos@ualberta.ca>
2020-08-25 11:32:23 -0600

    build: enable livereload

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,12 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 
 
 const production = !process.env.ROLLUP_WATCH;
+const watchMode = !production;
 
 export default {
   input: 'src/main.js',
@@ -19,8 +21,18 @@ export default {
       dev: !production,
     }),
 
-    resolve(),
+    resolve({
+      dedupe: ['svelte'],
+    }),
     commonjs(),
+
+    // Start the server when run as:
+    //    rollup -c -w
+    // (server starts after bundle has been generated for the first time).
+    watchMode && serve(),
+
+    // Refreshes the browser when anything in "public" has changed:
+    watchMode && livereload('public'),
 
     // Minify in production
     production && terser()
@@ -29,3 +41,29 @@ export default {
     clearScreen: false
   },
 };
+
+/**
+ * Livereloading server; uses `sirv-cli`
+ */
+function serve() {
+  let server;
+
+  function stopServer() {
+    if (server) server.kill(0);
+  }
+
+  return {
+    // Start the server on the first write to the bundle
+    writeBundle() {
+      if (server) return;
+      // runs sirv --dev (ignores cache headers)
+      server = require('child_process').spawn('yarn', ['run', 'start', '--dev'], {
+        stdio: [/*stdin */ 'ignore', /* stdout */ 'inherit', /* stderr */ 'inherit'],
+        shell: true
+      });
+
+      process.on('SIGTERM', stopServer);
+      process.on('exit', stopServer);
+    }
+  };
+}
