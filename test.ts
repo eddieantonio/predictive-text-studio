e17430f653778a9e48d7697ffd90047e97db0287
Eddie Antonio Santos <easantos@ualberta.ca>
2020-08-26 15:36:49 -0600

    build: setup paths so that tests work properly

--- test.ts
+++ test.ts
@@ -1,6 +1,6 @@
 import test from 'ava';
 
-import {theAnswerToLifeTheUniverseAndEverything} from './src/worker/placeholder';
+import {theAnswerToLifeTheUniverseAndEverything} from '@worker/placeholder';
 
 test('it has the answer to life, the universe, and everything', t => {
   t.is(theAnswerToLifeTheUniverseAndEverything(), 42);
