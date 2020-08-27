545e1c0982b8e47d667d1794282c498ba881306e
Eddie Antonio Santos <easantos@ualberta.ca>
2020-08-27 10:53:38 -0600

    build: add and configure prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,17 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: "module",
   },
   extends: [
     "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
   ],
   rules: {
     // Project-speific rules
     //
     // For now, there are none!
-  }
+  },
 };
---
+++ .prettierrc.js
@@ -0,0 +1,22 @@
+/**
+ * A summary of the code style:
+ *
+ *  - lines SHOULD be at most 80 characters long; some exceptions are welcome.
+ *  - 2 space indent, which makes the line length requirement more doable.
+ *  - semicolons are REQUIRED after every line.
+ *  - always use a comma at the end of multi-line literals:
+ *
+ *      {
+ *        "hello": "world", // ← comma!
+ *      }
+ *
+ *    (makes for smaller git diffs!)
+ *  - Use double-quotes for string literals UNLESS you're using double quotes
+ *    within the strings.
+ */
+module.exports = {
+  semi: true,
+  trailingComma: "es5",
+  printWidth: 80,
+  tabWidth: 2
+};
