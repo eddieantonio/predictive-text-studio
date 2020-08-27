dfe0aca6f66d3a013ca87735adf02498802200bb
Eddie Antonio Santos <easantos@ualberta.ca>
2020-08-27 10:53:56 -0600

    style: apply prettier 💅

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,16 +1,16 @@
-import App from './App.svelte';
+import App from "./App.svelte";
 
 const app = new App({
   target: document.body,
   props: {
-    name: 'Predictive Text Studio',
-  }
+    name: "Predictive Text Studio",
+  },
 });
 
 // Just to ensure the web worker... works
-const w = new Worker('worker.js');
+const w = new Worker("worker.js");
 w.onmessage = function (event) {
   console.log(event.data);
-}
+};
 
 export default app;
--- test/test-example.ts
+++ test/test-example.ts
@@ -1,7 +1,7 @@
-import test from 'ava';
+import test from "ava";
 
-import {theAnswerToLifeTheUniverseAndEverything} from '@worker/placeholder';
+import { theAnswerToLifeTheUniverseAndEverything } from "@worker/placeholder";
 
-test('it has the answer to life, the universe, and everything', t => {
+test("it has the answer to life, the universe, and everything", (t) => {
   t.is(theAnswerToLifeTheUniverseAndEverything(), 42);
 });
