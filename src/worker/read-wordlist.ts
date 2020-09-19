c097d16befbfdcde231b648422c508f656451ea1
xuechun <noglues666@gmail.com>
2020-09-19 13:34:14 -0400

    prettier

--- src/worker/read-wordlist.ts
+++ src/worker/read-wordlist.ts
@@ -1,8 +1,6 @@
 import readXlsxFile from "read-excel-file";
 
-export function returnWordlist(
-  ExcelFile: Buffer
-): Promise<[string, number][]> {
+export function returnWordlist(ExcelFile: Buffer): Promise<[string, number][]> {
   let wordlist: [string, number][] = [];
 
   return readXlsxFile(ExcelFile).then((rows: unknown[]) => {
