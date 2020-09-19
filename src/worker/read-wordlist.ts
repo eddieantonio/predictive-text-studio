a2ffa5c5cac5a202a32e8c83124b58dbe251bf22
xuechun <noglues666@gmail.com>
2020-09-19 13:26:20 -0400

    change row type

--- src/worker/read-wordlist.ts
+++ src/worker/read-wordlist.ts
@@ -1,10 +1,12 @@
 import readXlsxFile from "read-excel-file";
 
-export function returnWordlist(ExcelFile: Buffer):  Promise<Array<[string, number]>> {
-    let wordlist: Array<[string, number]> = [];
-    
-    return readXlsxFile(ExcelFile).then((rows: any) => {
-        wordlist = wordlist.concat(rows);
-        return wordlist;
-    })
+export function returnWordlist(
+  ExcelFile: Buffer
+): Promise<Array<[string, number]>> {
+  let wordlist: Array<[string, number]> = [];
+
+  return readXlsxFile(ExcelFile).then((rows: unknown[]) => {
+    wordlist = wordlist.concat(rows as [string, number][]);
+    return wordlist;
+  });
 }
