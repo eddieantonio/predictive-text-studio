35afc8649797c15a997e6018a6047ddca5c69186
xuechun <noglues666@gmail.com>
2020-09-19 12:42:28 -0400

    add return wordlist example

--- test/test-return-wordlist.ts
+++ test/test-return-wordlist.ts
@@ -4,5 +4,5 @@ import * as fs from "fs";
 import { returnWordlist } from "@worker/read-wordlist";
 
 test("it should return a wordlist given an Excel file", (t) => {
-  t.is(returnWordlist(fs.readFileSync("../ExampleWordlist")), []);
+  t.is(returnWordlist(fs.readFileSync("test/ExampleWordlist.xlsx")), [['TŦE',13644], ['E', 9134],['SEN',4816],['Ȼ',3479],['SW̱',2621],['NIȽ',2314],['U¸',2298],['I¸',1988],['ȻSE',1925],['I',1884]]);
 });
