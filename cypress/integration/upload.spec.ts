1db4fd5850d668216f98dcd548fad22684401f0b
Ian Seng <ianseng@outlook.com>
2020-09-19 12:04:12 -0600

    testing husky

--- cypress/integration/upload.spec.ts
+++ cypress/integration/upload.spec.ts
@@ -1,7 +1,7 @@
 describe("Upload component test cases", function () {
   it("should find an upload message on the homepage", function () {
     cy.visit("/");
-    cy.visit("hello")
+
     cy.contains("p", "Drag file to upload ...");
   });
 });
