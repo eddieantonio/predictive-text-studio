65cc697c1f77bf232e8fe618e23dc0b5e701cefd
Eddie Antonio Santos <easantos@ualberta.ca>
2020-09-21 15:18:06 -0600

    fix: forgot to change this import >.<

--- src/worker/main.ts
+++ src/worker/main.ts
@@ -1,5 +1,5 @@
-import { readExcelSync } from "./read-wordlist";
+import { readExcel } from "./read-wordlist";
 
 // Print the function's name (even though that's self-evident) just to get the
 // linter and Rollup to acknowledge that readExcelSync() is used.
-self.postMessage(`hello, from worker! I can use ${readExcelSync.name}`);
+self.postMessage(`hello, from worker! I can use ${readExcel.name}`);
