916d2c3d2408cf7f6a260fe0a51b8da58aaaa406
Eddie Antonio Santos <easantos@ualberta.ca>
2020-09-20 18:26:53 -0600

    docs(worker): write JSDoc for createZipWithFiles()

--- src/worker/generate-zip.ts
+++ src/worker/generate-zip.ts
@@ -9,6 +9,16 @@ export function generateZip(zip: JSZip): Promise<Buffer> {
  */
 type FileList = { [filename: string]: string };
 
+/**
+ * Given a series of filename/content pairs, creates a .zip file.
+ *
+ * For create a .kmp file, do the following:
+ *
+ *  let kmpFile = async createZipWithFiles({
+ *      [`${modelID}.model.js`]: compiledModelCode,
+ *      "kmp.json": JSON.stringify(modelInfo)
+ *  })
+ */
 export function createZipWithFiles(files: FileList): Promise<ArrayBuffer> {
   const zip = new JSZip();
   for (const [filename, contents] of Object.entries(files)) {
