0b7ad75d7e8d1ab20db8babdebd0d6c05e0f9584
Eddie Antonio Santos <easantos@ualberta.ca>
2020-09-21 09:35:08 -0600

    refactor(test): factor out example wordlist

--- test/test-return-wordlist.ts
+++ test/test-return-wordlist.ts
@@ -5,33 +5,29 @@ import * as path from "path";
 import { returnWordlist, readExcel } from "@worker/read-wordlist";
 
 const exampleExcelFilePath = path.join(__dirname, "ExampleWordlist.xlsx");
+const exampleWordlist = [
+  ["TŦE", 13644],
+  ["E", 9134],
+  ["SEN", 4816],
+  ["Ȼ", 3479],
+  ["SW̱", 2621],
+  ["NIȽ", 2314],
+  ["U¸", 2298],
+  ["I¸", 1988],
+  ["ȻSE", 1925],
+  ["I", 1884],
+];
 
 test("it should return a wordlist given an Excel file [using read-excel-file]", async (t) => {
-  t.deepEqual(await returnWordlist(fs.readFileSync(exampleExcelFilePath)), [
-    ["TŦE", 13644],
-    ["E", 9134],
-    ["SEN", 4816],
-    ["Ȼ", 3479],
-    ["SW̱", 2621],
-    ["NIȽ", 2314],
-    ["U¸", 2298],
-    ["I¸", 1988],
-    ["ȻSE", 1925],
-    ["I", 1884],
-  ]);
+  t.deepEqual(
+    await returnWordlist(fs.readFileSync(exampleExcelFilePath)),
+    exampleWordlist
+  );
 });
 
 test("it should return a wordlist given an Excel file [using xlsx]", async (t) => {
-  t.deepEqual(await readExcel(fs.readFileSync(exampleExcelFilePath)), [
-    ["TŦE", 13644],
-    ["E", 9134],
-    ["SEN", 4816],
-    ["Ȼ", 3479],
-    ["SW̱", 2621],
-    ["NIȽ", 2314],
-    ["U¸", 2298],
-    ["I¸", 1988],
-    ["ȻSE", 1925],
-    ["I", 1884],
-  ]);
+  t.deepEqual(
+    await readExcel(fs.readFileSync(exampleExcelFilePath)),
+    exampleWordlist
+  );
 });
