d56941aee5ae98129dee941f2d478e60918ea41f
Eddie Antonio Santos <easantos@ualberta.ca>
2020-09-21 09:27:11 -0600

    test: readExcel() returns the same results

--- test/test-return-wordlist.ts
+++ test/test-return-wordlist.ts
@@ -1,9 +1,9 @@
 import test from "ava";
 import * as fs from "fs";
 
-import { returnWordlist } from "@worker/read-wordlist";
+import { returnWordlist, readExcel } from "@worker/read-wordlist";
 
-test("it should return a wordlist given an Excel file", async (t) => {
+test("it should return a wordlist given an Excel file [using read-excel-file]", async (t) => {
   t.deepEqual(
     await returnWordlist(fs.readFileSync("test/ExampleWordlist.xlsx")),
     [
@@ -19,4 +19,19 @@ test("it should return a wordlist given an Excel file", async (t) => {
       ["I", 1884],
     ]
   );
+});
+
+test("it should return a wordlist given an Excel file [using xlsx]", async (t) => {
+  t.deepEqual(await readExcel(fs.readFileSync("test/ExampleWordlist.xlsx")), [
+    ["TŦE", 13644],
+    ["E", 9134],
+    ["SEN", 4816],
+    ["Ȼ", 3479],
+    ["SW̱", 2621],
+    ["NIȽ", 2314],
+    ["U¸", 2298],
+    ["I¸", 1988],
+    ["ȻSE", 1925],
+    ["I", 1884],
+  ]);
 });
