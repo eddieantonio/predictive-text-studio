2e804bb9ab277a9d528690ccb5f9b2cbe520dea3
Eddie Antonio Santos <easantos@ualberta.ca>
2020-09-20 18:19:36 -0600

    test: createZipWithFiles

--- src/worker/generate-zip.ts
+++ src/worker/generate-zip.ts
@@ -3,3 +3,12 @@ import * as JSZip from "jszip";
 export function generateZip(zip: JSZip): Promise<Buffer> {
   return zip.generateAsync({ type: "nodebuffer" });
 }
+
+/**
+ * Given a list of filenames and string contents, generates a ZIP file.
+ */
+type FileList = { [filename: string]: string };
+
+export function createZipWithFiles(files: FileList): Promise<ArrayBuffer> {
+  return Promise.reject(new Error("not implemented"));
+}
