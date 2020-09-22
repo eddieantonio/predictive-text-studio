00af826d3a7fabcf52ed3ffc09969997f77ba018
Eddie Antonio Santos <easantos@ualberta.ca>
2020-09-21 20:05:00 -0600

    remove(worker): placeholder test and implementation

--- test/test-example.ts
+++
@@ -1,7 +0,0 @@
-import test from "ava";
-
-import { theAnswerToLifeTheUniverseAndEverything } from "@worker/placeholder";
-
-test("it has the answer to life, the universe, and everything", (t) => {
-  t.is(theAnswerToLifeTheUniverseAndEverything(), 42);
-});
