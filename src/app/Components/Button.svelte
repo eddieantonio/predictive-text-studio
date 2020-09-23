f95a4b9162c3addef3a604b0d74d72502d051eac
Pranav Narang <pnarang1130@gmail.com>
2020-09-22 23:42:30 -0600

    Started working on the button component

---
+++ src/app/Components/Button.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  let color: string = "grey";
+  let isOutlined: boolean = false;
+  export let text: string = "";
+  export let callbackFunction: any = null;
+  export let className: string = "";
+
+  /**
+   * Creates the class name for the button if the styling props are passed
+   *
+   * @return {void}
+   */
+  const createStyles = (): void => {
+    className = "";
+    if (isOutlined) {
+      className += " outlined";
+    }
+
+    className += color;
+  };
+
+  // If the color or outlined props are passed in, create custom styles. Else the default (grey, false) values will be used for the button.
+  if (color || isOutlined) {
+    createStyles();
+  }
+</script>
+
+<button on:click={callbackFunction} class={className}> {text} </button>
