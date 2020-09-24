71a12a06963dd98e3d57037fe71388bdf0f0f56c
Pranav Narang <pnarang1130@gmail.com>
2020-09-23 23:15:59 -0600

    Started to refactor styling for button component's styling

--- src/app/Components/Button.svelte
+++ src/app/Components/Button.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
-  let color: string = "grey";
-  let isOutlined: boolean = false;
-  export let text: string = "";
-  export let callbackFunction: any = null;
+  // props
+  let color: string = "grey"; // optional
+  let isOutlined: boolean = false; // optional
+  let hasDropShadow: boolean = false; // optional
+  let size: string = "medium"; // optional
+  export let text: string = ""; // required
+  export let callbackFunction: any = null; // required
+
   export let className: string = "";
 
   /**
@@ -25,4 +29,68 @@
   }
 </script>
 
-<button on:click={callbackFunction} class={className}> {text} </button>
+<style>
+  button {
+    margin: 0px 10px;
+    border-radius: 10px;
+    text-transform: uppercase;
+    font-family: Cabin, sans-serif;
+    font-size: 15px;
+    font-weight: bold;
+    transition-duration: 0.2s;
+  }
+
+  button:hover {
+    cursor: pointer;
+  }
+
+  .small {
+    padding: 5px 10px;
+  }
+
+  .medium {
+    padding: 10px 20px;
+  }
+
+  .large {
+    padding: 20px 40px;
+  }
+
+  .grey {
+    background-color: #bdbdbd;
+  }
+
+  .grey:hover {
+    background-color: #c5c5c5;
+  }
+
+  .grey.outline {
+    border: 1px solid #bdbdbd;
+    background-color: #ffffff;
+  }
+
+  .grey.outline:hover {
+    background-color: #f1f1f1;
+  }
+
+  .blue {
+    background-color: #0099ff;
+  }
+
+  .blue:hover {
+    background-color: #006db6;
+  }
+
+  .blue.outline {
+    border: 1px solid #0099ff;
+    background-color: #ffffff;
+  }
+
+  .blue.outline:hover {
+    background-color: #f1f1f1;
+  }
+</style>
+
+{#if text && callbackFunction}
+  <button on:click={callbackFunction} class={className}> {text} </button>
+{/if}
