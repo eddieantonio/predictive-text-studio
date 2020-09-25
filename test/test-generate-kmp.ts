622c6d2a9eb4a7e279b1ef449957ddb0d41bab46
xuechun <noglues666@gmail.com>
2020-09-25 19:34:19 -0400

    change modelname to modelid

--- test/test-generate-kmp.ts
+++ test/test-generate-kmp.ts
@@ -2,12 +2,12 @@ import { generateKmp } from "@worker/generate-kmp";
 import test from "ava";
 import * as fs from "fs";
 
-const modelName = "nrc.en.mtnt.model.js";
+const modelID = "nrc.en.mtnt";
 const path = "test/fixtures/nrc.en.mtnt.model/";
-const contentModelJs = fs.readFileSync(path + modelName, "utf8");
+const contentModelJs = fs.readFileSync(path + modelID + ".model.js", "utf8");
 const contentKmpJson = fs.readFileSync(path + "kmp.json", "utf8");
 
 test("it should generate a kmp file", async (t) => {
-  const kmp = await generateKmp(modelName, contentModelJs, contentKmpJson);
+  const kmp = await generateKmp(modelID, contentModelJs, contentKmpJson);
   t.assert(kmp.byteLength > 0);
 });
