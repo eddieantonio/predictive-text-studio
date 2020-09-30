a4160ecdacf8c0dac37b65d3b7bd3fe0ff3399f1
Eddie Antonio Santos <easantos@ualberta.ca>
2020-09-29 19:31:26 -0600

    change(worker): deduplicate WordList type

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,10 +1,14 @@
 /**
- * Pairs a word with its absolute frequency (count) in a corpus.
+ * A pair of a word and its count (how many times it has occurred in some
+ * collection of text).
  */
 export type WordAndCount = [string, number];
 
 /**
- * An array of word counts.
+ * A collection of words and their counts.
+ *
+ * This list can be used to create a
+ * unigram language model (can provide current word completion, autocorrection).
  *
  * Ideally, this array should have **unique** members, and there is no
  * significance to the order of the members, however, this is not enforced by
