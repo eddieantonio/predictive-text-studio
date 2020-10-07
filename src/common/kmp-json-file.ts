28c5fea4030c1206722a92840fedb89e0b2ff88c
EstelleQ <44105167+Xuechunqiu@users.noreply.github.com>
2020-10-07 12:25:22 -0400

    Update src/common/kmp-json-file.ts
    Co-authored-by: Eddie Antonio Santos <easantos@ualberta.ca>

--- src/common/kmp-json-file.ts
+++ src/common/kmp-json-file.ts
@@ -41,7 +41,7 @@ export interface RelevantKmpOptions {
    * The model Id.
    * keyman will create a model Id for every models, derived from "organization, name of language, modelname"
    */
-  modelId?: string;
+  modelID?: string;
 }
 
 /**
