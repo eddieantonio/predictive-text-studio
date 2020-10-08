950c3c911bd97d09dc2b92db99a91405bacbf23e
Eddie Antonio Santos <easantos@ualberta.ca>
2020-10-08 16:04:19 -0600

    feat(app): add routes.ts

---
+++ src/app/routes.ts
@@ -0,0 +1,11 @@
+/**
+ * Maps names to URLs (routes) used in the app.
+ *
+ * Referring to the names rather than hardcoding the URLs keeps things DRY.
+ */
+const routes = {
+  landingPage: "/",
+  projectInfo: "/project/info",
+};
+
+export default routes;
