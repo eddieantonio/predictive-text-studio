67ca5b7a135809a433d62753a0c8d084a501cfe3
Eddie Antonio Santos <easantos@ualberta.ca>
2020-10-15 09:35:44 -0600

    test(app): can upload file and click download link

--- cypress/integration/upload.spec.ts
+++ cypress/integration/upload.spec.ts
@@ -1,9 +1,25 @@
-describe("Upload component test cases", function () {
-  it("should find an upload message on the homepage", function () {
+import "cypress-file-upload";
+
+describe("Upload from the the landing page", function () {
+  it("should find a button to press to upload a file", function () {
     cy.visit("/");
 
-    cy.contains("span", "Drag and drop here");
-    cy.contains("span", "or");
-    cy.contains("label", "Browse file");
+    cy.get("[data-cy=quick-start]")
+      .as("quick-start")
+      .scrollIntoView()
+      .contains("Browse file");
+
+    cy.get("@quick-start")
+      .get("[data-cy=download-kmp]")
+      .as("download-kmp")
+      .should("have.attr", "data-download-state", "disabled");
+
+    cy.get("@quick-start")
+      .get("[data-cy=upload-spreadsheet]")
+      .attachFile("sencoten-top-10.xlsx");
+
+    cy.get("@download-kmp")
+      .should("have.attr", "data-download-state", "ready")
+      .click();
   });
 });
