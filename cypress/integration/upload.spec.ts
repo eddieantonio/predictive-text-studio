9b29d6f5006dfe5377807e81d6e1c6f6154129da
Eddie Antonio Santos <easantos@ualberta.ca>
2020-10-15 10:06:11 -0600

    test(app): download to a temporary directory

--- cypress/integration/upload.spec.ts
+++ cypress/integration/upload.spec.ts
@@ -1,18 +1,44 @@
 import "cypress-file-upload";
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
+
+const downloadFolder = "cypress/downloads";
+
 describe("Upload from the the landing page", function () {
+  beforeEach(() => {
+    cy.task("clearDownloads");
+
+    // Allow downloads in Chromium-based browsers without prompts:
+    // Adapted from: https://github.com/cypress-io/cypress-example-recipes/blob/0b2ded6d7f099bb6e9e637fb45b1560879424bd5/examples/testing-dom__download/cypress/integration/spec.js#L12-L23
+    if (Cypress.browser.name !== "firefox") {
+      cy.wrap(
+        Cypress.automation("remote:debugger:protocol", {
+          command: "Page.setDownloadBehavior",
+          params: { behavior: "allow", downloadPath: downloadFolder },
+        }),
+        { log: false }
+      );
+    }
+  });
+
   it("should find a button to press to upload a file", function () {
     cy.visit("/");
+
+    const downloadedFilePath = path.join(downloadFolder, "Example.kmp");
 
     cy.get("[data-cy=quick-start]")
       .as("quick-start")
       .scrollIntoView()
       .contains("Browse file");
 
+    // Before we upload anything there should be no download link,
+    // and no file downloaded in our folder
     cy.get("@quick-start")
       .get("[data-cy=download-kmp]")
       .as("download-kmp")
       .should("have.attr", "data-download-state", "disabled");
+    cy.readFile(downloadedFilePath).should("not.exist");
 
     cy.get("@quick-start")
       .get("[data-cy=upload-spreadsheet]")
@@ -21,5 +47,7 @@ describe("Upload from the the landing page", function () {
     cy.get("@download-kmp")
       .should("have.attr", "data-download-state", "ready")
       .click();
+
+    cy.readFile(downloadedFilePath);
   });
 });
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -9,13 +9,54 @@
 // https://on.cypress.io/plugins-guide
 // ***********************************************************
 
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const fs = require("fs");
+
+// Save downloads in cypress/downloads/
+const downloadDirectory = path.join(__dirname, "..", "downloads");
 
 /**
+ * This function is called when a project is opened or re-opened (e.g. due to
+ * the project's config changing)
  * @type {Cypress.PluginConfig}
  */
-module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+
+module.exports = (on) => {
+  /**
+   * Adapted from: https://docs.cypress.io/api/plugins/browser-launch-api.html#Change-download-directory
+   */
+  on("before:browser:launch", (browser, options) => {
+    if (browser.family === "chromium" && browser.name !== "electron") {
+      options.preferences.default["download"] = {
+        default_directory: downloadDirectory,
+      };
+
+      return options;
+    }
+
+    if (browser.family === "firefox") {
+      options.preferences["browser.download.dir"] = downloadDirectory;
+      options.preferences["browser.download.folderList"] = 2;
+
+      // needed to prevent download prompt for text/csv files:
+      options.preferences["browser.helperApps.neverAsk.saveToDisk"];
+
+      return options;
+    }
+  });
+
+  on("task", {
+    /**
+     * Adapted from: https://github.com/cypress-io/cypress-example-recipes/blob/0b2ded6d7f099bb6e9e637fb45b1560879424bd5/examples/testing-dom__download/cypress/plugins/index.js#L22-L28
+     */
+    clearDownloads() {
+      console.log("clearing folder %s", downloadDirectory);
+
+      fs.rmdirSync(downloadDirectory, { recursive: true });
+
+      return null;
+    },
+  });
 };
