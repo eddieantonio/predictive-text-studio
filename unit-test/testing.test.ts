661b39bc2aa01080af9cfea338301e6a813154c7
Ian Seng <ianseng@outlook.com>
2020-10-19 09:25:14 -0600

    add comment

--- unit-test/testing.test.ts
+++ unit-test/testing.test.ts
@@ -1,5 +1,5 @@
 import { expect } from "chai";
-
+// TODO: Remvoe this file once we have other testing files
 describe("Options tests", () => {
   it("checking default options", () => {
     expect(true).to.be.true;
