85daec3ee77f11ef5d00aaab5cca964741bcabca
xuechun <noglues666@gmail.com>
2020-10-29 00:19:58 -0400

    add test for setProjectData()

--- test/test-worker.ts
+++ test/test-worker.ts
@@ -1,9 +1,11 @@
 import test from "ava";
 import * as sinon from "sinon";
 import { PredictiveTextStudioWorkerImpl } from "@worker/predictive-text-studio-worker-impl";
-import Storage from "@worker/storage";
+import Storage, { StoredProjectData } from "@worker/storage";
 import { StoredWordList } from "@worker/storage";
 import * as compiler from "@predictive-text-studio/lexical-model-compiler";
+import { exampleWordlist } from "./fixtures";
+import { WordList } from "@common/types";
 
 test("compile model should throws error when no file is found in the IndexedDB", async (t) => {
   const storageStub = new Storage();
@@ -38,4 +40,21 @@ test("compile model", async (t) => {
 
   compilerMock.verify();
   t.pass();
+});
+
+test("it should set project data and update to the database", async (t) => {
+  const testStoredProjectData = {
+    id: 1,
+    langName: "English",
+    bcp47Tag: "en",
+    authorName: "UnknownAuthor",
+  } as StoredProjectData;
+  const storageStub = new Storage();
+  const workerWrapper = new PredictiveTextStudioWorkerImpl(storageStub);
+  const metadata = { languages: [{ name: "English", id: "en" }] };
+  workerWrapper.setProjectData(metadata);
+  sinon
+    .stub(storageStub, "fetchProjectData")
+    .returns(Promise.resolve(testStoredProjectData));
+  t.is(await storageStub.fetchProjectData(), testStoredProjectData);
 });
