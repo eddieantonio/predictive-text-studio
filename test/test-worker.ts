95645b84fcc201457076989b56b07ea2d7260c2e
Eddie Antonio Santos <easantos@ualberta.ca>
2020-11-03 14:09:50 -0700

    remove(test): compile model tests

--- test/test-worker.ts
+++ test/test-worker.ts
@@ -2,43 +2,6 @@ import test from "ava";
 import * as sinon from "sinon";
 import { PredictiveTextStudioWorkerImpl } from "@worker/predictive-text-studio-worker-impl";
 import Storage, { StoredProjectData } from "@worker/storage";
-import { StoredWordList } from "@worker/storage";
-import * as compiler from "@predictive-text-studio/lexical-model-compiler";
-
-test("compile model should throws error when no file is found in the IndexedDB", async (t) => {
-  const storageStub = new Storage();
-  sinon.stub(storageStub, "fetchAllFiles").returns(Promise.resolve([]));
-
-  const workerWrapper = new PredictiveTextStudioWorkerImpl(storageStub);
-
-  const error = await t.throwsAsync(workerWrapper.compileModel());
-  t.is(error.message, "Cannot find any file in the IndexedDB");
-});
-
-test("compile model", async (t) => {
-  const exampleDictionarySource: StoredWordList = {
-    id: 1,
-    name: "test",
-    wordlist: [["test", 1]],
-  };
-
-  const storageStub = new Storage();
-  sinon
-    .stub(storageStub, "fetchAllFiles")
-    .returns(Promise.resolve([exampleDictionarySource]));
-  const compilerMock = sinon.mock(compiler);
-  compilerMock
-    .expects("WordListFromArray")
-    .once()
-    .withArgs(exampleDictionarySource.name, exampleDictionarySource.wordlist);
-  compilerMock.expects("compileModelFromLexicalModelSource").once();
-
-  const workerWrapper = new PredictiveTextStudioWorkerImpl(storageStub);
-  await workerWrapper.compileModel();
-
-  compilerMock.verify();
-  t.pass();
-});
 
 test("it should set project data and update to the database", async (t) => {
   const testStoredProjectData = {
