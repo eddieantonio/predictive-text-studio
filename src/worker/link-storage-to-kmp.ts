2724adada0e774c4b149d9a1f4cb5be1426da1e5
Eddie Antonio Santos <easantos@ualberta.ca>
2020-11-03 13:59:35 -0700

    change(worker): make code more concise

--- src/worker/link-storage-to-kmp.ts
+++ src/worker/link-storage-to-kmp.ts
@@ -1,12 +1,9 @@
-import { WordListFromArray } from "@predictive-text-studio/lexical-model-compiler";
 import { generateKmp } from "./generate-kmp";
 import Storage from "./storage";
 import { generateSourcesFromStorage } from "./generate-sources-from-storage";
 
 export async function linkStorageToKmp(storage: Storage): Promise<ArrayBuffer> {
-  const sources: WordListFromArray[] = await generateSourcesFromStorage(
-    storage
-  );
+  const sources = await generateSourcesFromStorage(storage);
 
   let projectData;
   try {
@@ -16,12 +13,8 @@ export async function linkStorageToKmp(storage: Storage): Promise<ArrayBuffer> {
       "Could not fetch the package info; Did you update the package BCP-47 tag?"
     );
   }
-  const langName = maybeProjectData.langName;
-  const bcp47Tag = maybeProjectData.bcp47Tag;
-  const authorName = maybeProjectData.authorName;
+  const { langName, bcp47Tag, authorName } = projectData;
   const modelID = `${authorName}.${bcp47Tag}.example`;
 
-  const kmpFile = await generateKmp(langName, bcp47Tag, sources, modelID);
-
-  return kmpFile;
+  return generateKmp(langName, bcp47Tag, sources, modelID);
 }
