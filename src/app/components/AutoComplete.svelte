7075d3f8f82ce255a82c965e9eb4d33467f8fd9d
Ian Seng <ianseng@outlook.com>
2020-11-09 12:48:27 -0700

    able to search now

--- src/app/components/AutoComplete.svelte
+++ src/app/components/AutoComplete.svelte
@@ -1,15 +1,28 @@
 <script lang="ts">
-  export let results: any = ["1111", "1122", "222"];
+  // Testing Data
+  export let results: any = [
+    {
+      bcp47Tag: "en",
+      langauge: "english",
+    },
+  ];
+  // To Store Filted array
   let filetred: any[] = [];
   let selected: any = "";
+  // Toggle to show search list
   let show = false;
+  // Previous focus list
   let prev = -1;
+
   let i = 0;
+
+  // Input to search
   function onChange(event: any) {
     show = true;
     filetred = results.filter((item: any) => {
-      let a = new RegExp("^" + event.target.value);
-      return a.test(item);
+      console.log(item.langauge);
+      let a = new RegExp("^" + event.target.value.toUpperCase());
+      return a.test(item.langauge.toUpperCase());
     });
   }
   function toggle() {
@@ -69,32 +82,65 @@
         console.log(i);
       }
     }
-
-    // current.blur();
-    //current.classList.add("autocomplete-active");
   }
 </script>
 
 <style>
   li:hover {
     cursor: pointer;
-    background: red;
+    background-color: var(--gray-highlight);
   }
   :global(.active) {
     /*when navigating through the items using the arrow keys:*/
-    background-color: DodgerBlue !important;
-    color: #ffffff;
+    margin: 0;
+    background-color: var(--gray-highlight);
+  }
+
+  input {
+    font-family: var(--secondary-font), sans-serif;
+    font-size: 18px;
+    padding: 10px;
+    border-radius: 10px;
+    border-width: 1px;
+    border-color: rgba(0, 0, 0, 0.2);
+  }
+
+  ul {
+    position: absolute;
+    padding: 0 0 10px 0;
+    margin: 0;
+    width: 100%;
+    background: white;
+    box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.2);
+    border-radius: 0 0 10px 10px;
+    list-style-type: none;
+    text-decoration: none;
+    z-index: 99;
+  }
+
+  li {
+    font-size: 18px;
+    font-style: bold;
+    padding: 10px 15px;
+    overflow: hidden;
+    border-right: solid 15px rgba(0, 0, 0, 0);
+  }
+  .main {
+    position: relative;
+    width: fit-content;
   }
 </style>
 
 <svelte:window on:keydown={handleKeydown} />
-<div>
+<div class="main">
   <input value={selected} type="text" on:input={(event) => onChange(event)} />
   {#if show}
     <ul use:clickOutside={toggle}>
       {#each filetred as result}
         <li class="item">
-          <div on:click={() => selectedList(result)}>{result}</div>
+          <div on:click={() => selectedList(result.langauge)}>
+            {result.langauge}
+          </div>
         </li>
       {/each}
     </ul>
