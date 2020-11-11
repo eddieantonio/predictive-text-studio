d646c87e4160fe37380061824ea60b6fe37b513d
Ian Seng <ianseng@outlook.com>
2020-11-10 22:51:22 -0700

    fix testing error

--- src/worker/models.ts
+++ src/worker/models.ts
@@ -6,7 +6,7 @@
  * KMP endpoint returning object type. Doc: https://help.keyman.com/developer/cloud/search/1.0/#toc-searchlanguage-object
  */
 export interface SearchLanguage {
-  keyboards: Array<string>;
+  keyboards: string[];
   id: string;
   name: string;
 }
