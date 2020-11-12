438c7f35e4f8b364b9d0cd75659c84eb60266648
xuechun <noglues666@gmail.com>
2020-11-12 12:35:31 -0500

    ajust data-cy attribute

--- src/app/pages/Languages.svelte
+++ src/app/pages/Languages.svelte
@@ -180,19 +180,19 @@
           color="grey"
           isOutlined={(selectedButton === 'information')}
           onClick={() => handleClick('information')}
-          data_cy="languages-information-btn"
+          dataCy="languages-information-btn"
         >Information</Button>
         <Button
           color="grey"
           isOutlined={(selectedButton === 'sources')}
           onClick={()=> handleClick('sources')}
-          data_cy="languages-sources-btn"
+          dataCy="languages-sources-btn"
         >Sources</Button>
         <Button
           color="blue"
           onClick={handleDownload}
           subtext={languageInformation.wordCount.toString() + " words"}
-          data_cy="languages-download-btn"
+          dataCy="languages-download-btn"
         >Download</Button>
       </div>
       <div class="languages__container--content">
