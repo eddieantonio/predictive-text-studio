94e26d84fdf73a6fd2d17624e39595ee095b878b
Pranav Narang <pnarang1130@gmail.com>
2020-11-12 23:07:43 -0700

    Added readGoogleSheet method to interface

--- src/common/predictive-text-studio-worker.ts
+++ src/common/predictive-text-studio-worker.ts
@@ -1,11 +1,18 @@
 /**
- * This interface establishes a communication protocal between the UI thread and the worker thread
+ * This interface establishes a communication protocol between the UI thread and the worker thread
  */
+
+import type { WordList } from "@common/types";
+
 export interface PredictiveTextStudioWorker {
   /**
    * Save a file into IndexedDB
    */
   saveFile(name: string, file: File): Promise<ArrayBuffer>;
+  /**
+   * Save a google sheet into IndexedDB
+   */
+  readGoogleSheet(name: string, wordListObject: WordList): Promise<ArrayBuffer>;
   /**
    * Compile the lexical model using files in the IndexedDB
    */
