a55084ce8aaea9b627bab8e6965aedf796b26c62
xuechun <noglues666@gmail.com>
2020-11-20 16:55:36 -0500

    merge conflict

--- src/app/components/AddSource.svelte
+++ src/app/components/AddSource.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
-  import worker from "../spawn-worker";
   import SplitButton from "./SplitButton.svelte";
   import ManualEntry from "./ManualEntry.svelte";
   const UPLOAD_INPUT_ID = "upload-input";
+  import Upload from "./Upload.svelte";
   let manualEntry: boolean = false;
-  let onDraggedOver = false;
-  let fileList: File[] | FileList = [];
   let tableData = {
     name: "",
     data: [
@@ -16,48 +14,7 @@
     ],
   };
 
-  function fileFromDataTransferItem(items: DataTransferItemList): File[] {
-    const fileList: File[] = [];
-    for (let item of items) {
-      // If dropped items aren't files, reject them
-      if (item.kind === "file") {
-        const file = item.getAsFile();
-        if (file !== null) {
-          fileList.push(file);
-        }
-      }
-    }
-    return fileList;
-  }
-
-  const handleDrop = (event: DragEvent) => {
-    onDraggedOver = false;
-
-    if (event.dataTransfer == null) {
-      return;
-    } else if (event.dataTransfer.items) {
-      // Use DataTransferItemList interface to access the file(s)
-      fileList = fileFromDataTransferItem(event.dataTransfer.items);
-    } else {
-      // Use DataTransfer interface to access the file(s)
-      fileList = Array.from(event.dataTransfer.files);
-    }
-  };
-
-  const handleDragOver = () => {
-    onDraggedOver = true;
-  };
-
-  const handleDragLeave = () => {
-    onDraggedOver = false;
-  };
-
-  const handleChange = (event: Event): void => {
-    const input = event.target as HTMLInputElement;
-    if (input !== null && input.files) {
-      fileList = input.files;
-    }
-  };
+  const saveAddedSources = (): void => {};
 
   const closeAddSourceZone = () => {
     manualEntry = false;
@@ -73,43 +30,6 @@
 </script>
 
 <style>
-  .upload-zone {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    padding: 5%;
-    min-width: 200px;
-    border-radius: 5px;
-    border: 3px dashed var(--gray-light);
-    line-height: 1.2;
-  }
-
-  .drag-over {
-    border-color: var(--blue);
-    background-color: var(--blue-light);
-  }
-
-  input {
-    display: none;
-  }
-
-  .upload-btn {
-    margin: 0 auto;
-    color: var(--primary-blue);
-  }
-
-  .upload-btn:hover {
-    cursor: pointer;
-    text-decoration: underline;
-  }
-
-  img {
-    width: 2em;
-    padding-bottom: 1rem;
-    color: var(--blue);
-  }
-
   .top-button-zone {
     display: flex;
     flex-direction: column;
@@ -132,21 +52,5 @@
 {#if manualEntry}
   <ManualEntry {tableData} />
 {:else}
-  <div
-    class="upload-zone"
-    class:drag-over={onDraggedOver}
-    on:drop|preventDefault={handleDrop}
-    on:dragover|preventDefault={handleDragOver}
-    on:dragleave|preventDefault={handleDragLeave}
-    data-cy="add-source-upload-zone">
-    <img role="presentation" src="icons/upload-solid.svg" alt="" />
-    <span>Drag and drop here</span>
-    <span>or</span>
-    <label for={UPLOAD_INPUT_ID} class="upload-btn">Browse file</label>
-    <input
-      id={UPLOAD_INPUT_ID}
-      type="file"
-      on:change={handleChange}
-      data-cy="upload-spreadsheet" />
-  </div>
+  <Upload />
 {/if}
