ee3900d9e20f8151bc8992de53de8d504c776bf1
Ian Seng <ianseng@outlook.com>
2020-11-24 09:33:03 -0700

    fix bug to disable click

--- src/app/components/DownloadKMP.svelte
+++ src/app/components/DownloadKMP.svelte
@@ -3,25 +3,32 @@
 </script>
 
 <style>
-  .download-link {
+  .download-component--disabled {
+    cursor: not-allowed;
+  }
+  .download-component__link {
     display: block;
     margin-top: 1.5em;
   }
 
-  .download-link--disabled {
+  .download-component__link--disabled {
     color: var(--gray-medium-dark);
-    cursor: not-allowed;
     opacity: 25%;
+    pointer-events: none;
     text-decoration: none;
   }
 </style>
 
-<a
-  href={downloadURL ? downloadURL : '#'}
-  download="Example.kmp"
-  class="download-link"
-  class:download-link--disabled={downloadURL == ''}
-  data-cy="download-kmp"
-  data-download-state={downloadURL ? 'ready' : 'disabled'}>
-  Download KMP Package
-</a>
+<div
+  class="download-component"
+  class:download-component--disabled={downloadURL == ''}>
+  <a
+    href={downloadURL ? downloadURL : '#'}
+    download="Example.kmp"
+    class="download-component__link"
+    class:download-component__link--disabled={downloadURL == ''}
+    data-cy="download-kmp"
+    data-download-state={downloadURL ? 'ready' : 'disabled'}>
+    Download KMP Package
+  </a>
+</div>
