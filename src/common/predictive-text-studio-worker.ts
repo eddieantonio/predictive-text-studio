8e4cf97ecc02ce13c27a36261f7a38a0d2ac1659
Eddie Antonio Santos <easantos@ualberta.ca>
2020-11-30 09:53:04 -0700

    refactor: use existing interface

--- src/common/predictive-text-studio-worker.ts
+++ src/common/predictive-text-studio-worker.ts
@@ -1,23 +1,6 @@
 import type { RelevantKmpOptions } from "./kmp-json-file";
-import type { DictionaryEntry } from "./types";
+import type { DictionaryEntry, KeyboardDataWithTime } from "./types";
 
-/**
- * KeyboardDataWithTime interface
- */
-interface KeyboardDataWithTime {
-  /**
-   * Store bcp47Tag as the primarykey
-   */
-  bcp47Tag: string;
-  /**
-   * Keyboard Language of the bcp47Tag
-   */
-  language: string;
-  /**
-   * Timestamp of last updated time
-   */
-  timestamp: Date;
-}
 /**
  * This interface establishes a communication protocal between the UI thread and the worker thread
  */
