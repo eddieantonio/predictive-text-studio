d46be7a7a6b0e6ab51cea44f2fb84bb16d1bf5c0
Eddie Antonio Santos <eddieantonio@hey.com>
2021-01-23 11:40:47 -0700

    test: fetchAllProjectMetadata

--- test/test-worker.ts
+++ test/test-worker.ts
@@ -2,7 +2,6 @@ import test from "ava";
 import * as sinon from "sinon";
 import { PredictiveTextStudioWorkerImpl } from "@worker/predictive-text-studio-worker-impl";
 import Storage from "@worker/storage";
-import { StoredProjectData } from "@worker/storage-models";
 global.fetch = require("node-fetch");
 
 test("it should set project data and update to the database", async (t) => {
@@ -13,24 +12,19 @@ test("it should set project data and update to the database", async (t) => {
   const languageTag = "moh";
   const authorName = "Aidan";
 
-  const testStoredProjectData = {
-    id: 1,
-    langName: languageName,
-    bcp47Tag: languageTag,
-    authorName: authorName,
-  } as StoredProjectData;
-
   const storageStub = storageWithStubbedKeyboardData();
-
-  sinon
-    .stub(storageStub, "fetchProjectData")
-    .returns(Promise.resolve(testStoredProjectData));
-
   const worker = new PredictiveTextStudioWorkerImpl(storageStub);
+
   worker.setProjectData({
     languages: [{ name: languageName, id: languageTag }],
   });
-  t.deepEqual(await storageStub.fetchProjectData(), testStoredProjectData);
+  worker.setProjectData({ authorName });
+
+  const data = await worker.fetchAllCurrentProjectMetadata();
+
+  t.is(data.langName, languageName);
+  t.is(data.bcp47Tag, languageTag);
+  t.is(data.authorName, authorName);
 });
 
 function storageWithStubbedKeyboardData() {
