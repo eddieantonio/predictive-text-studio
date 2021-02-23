ed58edc29a09291436ad797432201a42c81fc09a
ChrisChrisLoLo <sporewoh@gmail.com>
2021-02-23 00:37:53 -0700

    formatting

--- src/common/logging.ts
+++ src/common/logging.ts
@@ -1,26 +1,26 @@
 declare const DEVELOPMENT: boolean;
 
-let hasWarnedDev: boolean = false;
+let hasWarnedDev = false;
 
 /**
  * Log an error in development mode.
  * Will warn if the .env file doesn't have DEVELOPEMENT equal to 'true'.
- * 
+ *
  * Should be used to warn that something went wrong.
- * 
+ *
  * @param e the error object to include. A message can be included in the object.
  */
-function logError(e: Error): void{
+function logError(e: Error): void {
   if (DEVELOPMENT) {
     console.error(e);
   } else {
-    if(!hasWarnedDev){
+    if (!hasWarnedDev) {
       // Message warns the dev that logging is disabled
-      console.warn('Developer Logging Not Enabled')
+      console.warn("Developer Logging Not Enabled");
       hasWarnedDev = true;
     }
     // Do nothing
   }
 }
 
-export {logError};
+export { logError };
