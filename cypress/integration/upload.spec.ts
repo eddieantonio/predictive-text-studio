8cd626a2ebc9ae935885ac5fabcc2f37d8fa131f
ChrisChrisLoLo <sporewoh@gmail.com>
2021-04-08 17:02:03 -0600

    test

--- cypress/integration/upload.spec.ts
+++ cypress/integration/upload.spec.ts
@@ -16,45 +16,17 @@ describe("Upload from the the landing page and Download KMP", function () {
   });
 
   it("should find a button to press to upload a file", function () {
-    cy.intercept("https://cache.predictivetext.studio/cached-keyman-api.json", {
-      fixture: "response-keyman.json",
-    });
     // Load languages
     cy.visit("/");
-    cy.wait(1000);
-    // Render obtained languages
-    cy.visit("/");
-    cy.wait(1000);
+    cy.wait(500);
+
 
     cy.data("download-kmp")
       .as("download-kmp")
       .should("have.attr", "data-download-state", "disabled");
 
-    // Select the first option (should be Straits Salish)
-    cy.data("autocomplete-label").type("straits").type("{enter}");
-
     const downloadedFilePath = path.join(downloadFolder, "Example.kmp");
-
-    cy.data("quick-start")
-      .as("quick-start")
-      .scrollIntoView()
-      .contains("Browse file");
-
-    cy.readFile(downloadedFilePath).should("not.exist");
-
-    const filename = "sencoten-top-10.xlsx";
-    cy.fixture(filename, "base64").then((fixture) => {
-      const testFile = new File(
-        [Cypress.Blob.base64StringToBlob(fixture)],
-        filename
-      );
-      const event = { dataTransfer: { files: [testFile] } };
-
-      cy.get("@quick-start")
-        .data("upload-dropzone")
-        .trigger("dragenter", event);
-      cy.get("@quick-start").data("upload-dropzone").trigger("drop", event);
-    });
+    cy.generateProject();
 
     cy.wait(1000);
     cy.get("@download-kmp")
