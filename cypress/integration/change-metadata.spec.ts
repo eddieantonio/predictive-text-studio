4ea0be7b4c86f34e4e828b884c1bc59b906a48e9
Zachary Probst <probst.zap@gmail.com>
2021-04-13 13:57:47 -0400

    change language check

--- cypress/integration/change-metadata.spec.ts
+++ cypress/integration/change-metadata.spec.ts
@@ -38,20 +38,20 @@ describe("Changing metadata in the language info page", function () {
     languageInput().should("have.value", languageName);
     cy.data("input-author-name").should("have.value", authorName);
     cy.data("input-copyright").should("have.value", copyright);
+    cy.data("input-dictionary-name").should("have.value", dictionaryName);
   });
 
   it("should recompile KMP on metadata change", function () {
     cy.visit("/customize");
 
-    // Wait for the languages to load
-    cy.wait(2000);
-    languageInput().clear().type(languageName).type("{enter}");
+    cy.wait(500);
+
     cy.data("input-author-name").clear().type(authorName);
     cy.data("input-dictionary-name").clear().type(dictionaryName);
     cy.data("input-copyright").clear().type(copyright).blur();
     // Wait for compilation
     // TODO: can we avoid waiting here?
-    cy.wait(2000);
+    cy.wait(500);
 
     const downloadFolder = Cypress.env("downloadFolder");
     cy.task("clearDownloads");
@@ -63,16 +63,6 @@ describe("Changing metadata in the language info page", function () {
       .should("not.have.class", "button--disabled")
       .click();
 
-    const expectedLexicalModels = [
-      {
-        name: "’Are’are dictionary",
-        id: "Eddie.alu.example",
-        languages: [{ name: "’Are’are", id: "alu" }],
-      },
-    ];
-
-    cy.wait(100);
-
     //TODO: await instead
     cy.readZip(downloadedFilePath).then(async (zip) => {
       expect(zip.file("kmp.json")).to.not.be.null;
@@ -83,10 +73,11 @@ describe("Changing metadata in the language info page", function () {
       expect(kmpInfo.copyright.description).to.deep.equal(copyright);
       expect(kmpInfo.name.description).to.deep.equal(dictionaryName);
       expect(kmpInfo.author.description).to.deep.equal(authorName);
-      expect(kmpFile.lexicalModels).to.deep.equal(expectedLexicalModels);
 
       // change inputs and expect kmp to change
       cy.data("customize-information-btn").scrollIntoView().click();
+
+      languageInput().clear().type(languageName).type("{enter}").blur();
 
       cy.data("input-author-name")
         .clear()
@@ -100,6 +91,14 @@ describe("Changing metadata in the language info page", function () {
         .blur();
 
       cy.wait(200); // wait for compilation
+
+      const expectedLexicalModels = [
+        {
+          name: "’Are’are dictionary",
+          id: "new Eddie.alu.example",
+          languages: [{ name: "’Are’are", id: "alu" }],
+        },
+      ];
 
       const newDownloadedFilePath = path.join(
         downloadFolder,
@@ -125,6 +124,7 @@ describe("Changing metadata in the language info page", function () {
         expect(kmpInfoNew.author.description).to.deep.equal(
           "new " + authorName
         );
+        expect(kmpFileNew.lexicalModels).to.deep.equal(expectedLexicalModels);
         cy.data("input-dictionary-name").clear().blur(); // clear dictionary name to avoid affecting other tests
       });
     });
